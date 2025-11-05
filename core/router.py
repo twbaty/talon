eb83829da0d90152e64cf1a432c29ccf96d18a04
twbaty <51901395+twbaty@users.noreply.github.com>
2025-11-05 15:04:01 -0600

    Create router.py

---
+++ core/router.py
@@ -0,0 +1,15 @@
+# core/router.py
+
+from core import abuseipdb, shodan, censys, virustotal
+
+def scan_ip(service, ip):
+    if service == "AbuseIPDB":
+        return abuseipdb.check_ip(ip)
+    elif service == "Shodan":
+        return shodan.check_ip(ip)
+    elif service == "Censys":
+        return censys.check_ip(ip)
+    elif service == "VirusTotal":
+        return virustotal.check_ip(ip)
+    else:
+        return {"error": "Unknown service"}
