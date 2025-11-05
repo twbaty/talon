6ae5da5c4ae7d30f4980163e5b31c8d53a56e699
twbaty <51901395+twbaty@users.noreply.github.com>
2025-11-05 14:56:57 -0600

    Create main.py

---
+++ core/main.py
@@ -0,0 +1,13 @@
+# core/main.py
+
+from core import abuseipdb, shodan, censys, virustotal
+
+def scan_ip(ip_address):
+    results = {}
+
+    results["AbuseIPDB"] = abuseipdb.query_abuseipdb(ip_address)
+    results["Shodan"] = shodan.query_shodan(ip_address)
+    results["Censys"] = censys.query_censys(ip_address)
+    results["VirusTotal"] = virustotal.query_virustotal(ip_address)
+
+    return results
