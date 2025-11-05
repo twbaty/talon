17d70bcb79fd82d1fc29570f6348d6996e2356b6
twbaty <51901395+twbaty@users.noreply.github.com>
2025-11-05 15:05:26 -0600

    Update talon.py

--- talon.py
+++ talon.py
@@ -1,18 +1,4 @@
-from core import abuseipdb, shodan, censys, virustotal
-
-
-def scan_ip(service, ip):
-    if service == "AbuseIPDB":
-        return abuseipdb.check_ip(ip)
-    elif service == "Shodan":
-        return shodan.check_ip(ip)
-    elif service == "Censys":
-        return censys.check_ip(ip)
-    elif service == "VirusTotal":
-        return virustotal.check_ip(ip)
-    else:
-        return {"error": "Unknown service"}
-
+# talon.py
 from gui.main_window import launch_gui
 
 if __name__ == "__main__":
