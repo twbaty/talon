16b01c6190ab35cfb7ee58fdc3fec6b00d6bfaab
twbaty <51901395+twbaty@users.noreply.github.com>
2025-11-05 17:13:24 -0600

    Update logging.py

--- utils/logging.py
+++ utils/logging.py
@@ -1,1 +1,13 @@
+import logging
 
+logging.basicConfig(
+    filename='talon.log',
+    level=logging.INFO,
+    format='%(asctime)s %(levelname)s: %(message)s'
+)
+
+def log_error(msg):
+    logging.error(msg)
+
+def log_info(msg):
+    logging.info(msg)
