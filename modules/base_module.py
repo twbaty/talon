12c290e4a728db3fee895a9da31e9f7dd33eb572
twbaty <51901395+twbaty@users.noreply.github.com>
2025-11-05 12:43:24 -0600

    Update base_module.py

--- modules/base_module.py
+++ modules/base_module.py
@@ -1,1 +1,20 @@
+# recon_gui/modules/base_module.py
+
+from abc import ABC, abstractmethod
+
+class ReconModule(ABC):
+    @abstractmethod
+    def name(self) -> str:
+        """Return the name of the module."""
+        pass
+
+    @abstractmethod
+    def get_queries(self) -> dict:
+        """Return a dictionary of predefined queries with parameterized templates."""
+        pass
+
+    @abstractmethod
+    def render_query(self, template: str, params: dict) -> str:
+        """Substitute params into the template and return the final query string."""
+        pass
 
