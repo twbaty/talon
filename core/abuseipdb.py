f5808e2cf58bdde986472cf0dbb2276227a0064d
twbaty <51901395+twbaty@users.noreply.github.com>
2025-11-05 20:01:20 -0600

    Update abuseipdb.py

--- core/abuseipdb.py
+++ core/abuseipdb.py
@@ -9,8 +9,9 @@ ABUSEIPDB_API = "https://api.abuseipdb.com/api/v2/check"
 def query_abuseipdb(ip):
     api_key = os.getenv("ABUSEIPDB_API_KEY")
     if not api_key:
-        print(f"[INFO] No API key found. Opening AbuseIPDB page for {ip}")
-    return {"redirect": f"https://www.abuseipdb.com/check/{ip}"}
+       return {"error": "No AbuseIPDB API key found. Register for one or manually check: https://www.abuseipdb.com"}
+##    print(f"[INFO] No API key found. Opening AbuseIPDB page for {ip}")
+##    return {"redirect": f"https://www.abuseipdb.com/check/{ip}"}
 
     headers = {
         "Accept": "application/json",
