f97bcb2e119d887333924f722a379d0674a21e35
twbaty <51901395+twbaty@users.noreply.github.com>
2025-11-05 19:49:49 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # talon.py
 from gui.main_window import launch_gui
+import os
 from dotenv import load_dotenv
 load_dotenv()
+ABUSEIPDB_API_KEY = os.getenv("ABUSEIPDB_API_KEY")
 
 if __name__ == "__main__":
     launch_gui()
